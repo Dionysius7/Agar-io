9dd941c011d947b68ef8e54eaf4806f4f08c4245
Dionysius <jayasentausa@gmail.com>
2020-04-02 20:34:47 +0700

    Added perspective view

--- sketch.js
+++ sketch.js
@@ -5,13 +5,19 @@ var blobs = [];
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	blob = new Blob(width / 2, height / 2, 64);
-	for (var i = 0; i < 10; i++) {
-		blobs[i] = new Blob(random(width), random(height), 16);
+
+	for (var i = 0; i < 50; i++) {
+		var x = random(-width, width * 2);
+		var y = random(-height, height * 2);
+		blobs[i] = new Blob(x, y, 16);
 	}
 }
 
 function draw() {
 	background(0);
+
+	translate(width / 2 - blob.pos.x, height / 2 - blob.pos.y);
+
 	blob.show();
 	blob.update();
 	for (var i = 0; i < blobs.length; i++) {
