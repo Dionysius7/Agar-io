060f318c1c09afa72fb307d5a143a250336ab95e
Dionysius <jayasentausa@gmail.com>
2020-04-14 10:54:23 +0700

    Added Eating Feature and Lerp

--- blob.js
+++ blob.js
@@ -1,14 +1,32 @@
 function Blob(x, y, r) {
 	this.pos = createVector(x, y);
 	this.r = r;
+	this.velocity = createVector(0, 0);
 
 	this.update = function () {
-		var vel = createVector(mouseX - width / 2, mouseY - height / 2);
-		vel.setMag(3);
-		this.pos.add(vel);
+		// Create Direction
+		let newVelocity = createVector(mouseX - width / 2, mouseY - height / 2);
+
+		//Calculate speed level
+		newVelocity.setMag(10);
+		this.velocity.lerp(newVelocity, 0.01);
+
+		//Update the location
+		this.pos.add(this.velocity);
 	}
 
+	this.eat = function (other) {
+		let d = p5.Vector.dist(this.pos, other.pos);
+		if (d < this.r + other.r) {
+			//Calculate the combined radius
+			let newRadius = (PI * this.r * this.r) + (PI * other.r * other.r);
+			//update the blob radius
+			this.r = sqrt(newRadius / PI);
 
+			return true;
+		}
+		return false;
+	}
 
 	this.show = function () {
 		fill(255);
--- sketch.js
+++ sketch.js
@@ -1,26 +1,38 @@
 var blob;
+var reso_amount = 500;
+var zoom = 1;
 
-var blobs = [];
+var resources = [];
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	blob = new Blob(width / 2, height / 2, 64);
 
-	for (var i = 0; i < 50; i++) {
-		var x = random(-width, width * 2);
-		var y = random(-height, height * 2);
-		blobs[i] = new Blob(x, y, 16);
+	for (let i = 0; i < reso_amount; i++) {
+		let x = random(-width * 2, width * 2);
+		let y = random(-height * 2, height * 2);
+		resources[i] = new Blob(x, y, 16);
 	}
 }
 
 function draw() {
 	background(0);
 
-	translate(width / 2 - blob.pos.x, height / 2 - blob.pos.y);
+	//Perspective View
+	translate(width / 2, height / 2);
+	let newzoom = 64 / blob.r;
+	//Linear interpolated (lerp) to shrink world smoothly
+	zoom = lerp(zoom, newzoom, 0.1);
+	scale(zoom);
+	translate(-blob.pos.x, -blob.pos.y);
 
 	blob.show();
 	blob.update();
-	for (var i = 0; i < blobs.length; i++) {
-		blobs[i].show();
+
+	for (let i = 0; i < resources.length; i++) {
+		resources[i].show();
+		if (blob.eat(resources[i])) {
+			resources.splice(i, 1);
+		}
 	}
 }
